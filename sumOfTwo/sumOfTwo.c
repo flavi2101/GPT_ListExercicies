3977056a4cb0981402df98443d08379d60c00710
Flavio Henrique <flavi2101@gmail.com>
2023-07-03 19:33:14 -0300

    sum of two values

---
+++ sumOfTwo/sumOfTwo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main(){
+	int value1;
+	int value2;
+	printf("insert the firts value:");
+	scanf("%d",&value1);
+	printf("insert the second value:");
+	scanf("%d",&value2);
+	printf("sum of the values is %d", value1+value2);
+
+}
